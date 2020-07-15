c8c04ca9bd50918837605dcf5c2533f36a85c733
Aniket Sood <nyp.aniket@gmail.com>
2020-07-15 16:12:26 +0530

    angular edureka

---
+++ AT/src/app/mock-courses.ts
@@ -0,0 +1,11 @@
+import { Course } from './course';
+
+export const COURSES: Course[] = [
+    { id: 1, name: "C++" },
+    { id: 2, name: "Angular" },
+    { id: 3, name: "WEB DEV" },
+    { id: 4, name: "Java" },
+    { id: 5, name: "Python" },
+    { id: 6, name: "ML" },
+    { id: 7, name: "Spring Boot" }
+];
